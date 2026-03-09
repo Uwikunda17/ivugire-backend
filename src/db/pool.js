97b6af4f5ae543dcc2c241dab46012f02a9a71df
Uwikunda17 <uwikundac5@gmail.com>
2026-03-09 18:04:35 -0300

    update the pool.js to use the external url for postgresq

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,12 +1,17 @@
 const { Pool } = require('pg')
 
-const pool = new Pool({
-  host: process.env.PGHOST || 'localhost',
-  port: Number(process.env.PGPORT || 5432),
-  user: process.env.PGUSER || 'postgres',
-  password: process.env.PGPASSWORD || 'jules098765',
-  database: process.env.PGDATABASE || 'ivugire',
-  max: Number(process.env.PGPOOL_MAX || 10),
-})
+const pool = new Pool(
+  process.env.DATABASE_URL ? {
+    connectionString: process.env.DATABASE_URL,
+    max: Number(process.env.PGPOOL_MAX || 10),
+  } : {
+    host: process.env.PGHOST || 'localhost',
+    port: Number(process.env.PGPORT || 5432),
+    user: process.env.PGUSER || 'postgres',
+    password: process.env.PGPASSWORD || 'jules098765',
+    database: process.env.PGDATABASE || 'ivugire',
+    max: Number(process.env.PGPOOL_MAX || 10),
+  }
+)
 
 module.exports = { pool }
